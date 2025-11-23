47495099d7f5e4d7e1943d8363deae831dbc5aa3
keisukehayano <hayano.1989@gmail.com>
2025-11-23 18:32:15 +0900

    修正

--- internal/domain/repository/stream_repository.go
+++ internal/domain/repository/stream_repository.go
@@ -9,8 +9,7 @@ import (
 // StreamRepository はデータ永続化に関する契約(インターフェース)です。
 // 実装は、internal/infrastuctureで行います。
 type StreamRepository interface {
-	FindByKey(ctx context.Context, key string) (&entity.Stream)
-	Save(ctx context.Context, stram &entity.Stream)
+	FindByKey(ctx context.Context, key string) (*entity.Stream, error)
+	Save(ctx context.Context, stram *entity.Stream)
 	UpdateStatus(ctx context.Context, id string, stream entity.StreamStatus) error
-
 }
